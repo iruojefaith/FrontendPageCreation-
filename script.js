79081e60408efc3ae1a4d610d81946963d56894d
iruojefaith <faithiruoje@gmail.com>
2023-09-06 15:58:03 -0700

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateDateTime() {
+    const dayOfTheWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const UTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const now = new Date();
+    const dayOfWeek = daysOfWeek[now.getUTCDay()];
+    const UTCTime = now.toUTCString();
+
+    dayOfTheWeekElement.textContent = dayOfWeek;
+    UTCTimeElement.textContent = UTCTime;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 10000); // Update every 10 seconds
